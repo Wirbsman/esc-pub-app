30f5e889a56d3a3677c2bd8c48685d768a37cb93
Florian Wirbser <fwi@smotive.de>
2023-03-12 19:16:31 +0100

    - add functional navigation schematic - add material table for testing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {UsersComponent} from "./users/users.component";
+import {UserTableComponent} from "./user-table/user-table.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'users1', component: UsersComponent},
+  {path: 'users2', component: UserTableComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,17 +5,45 @@ import { AppComponent } from './app.component';
 import { HttpClientModule} from "@angular/common/http";
 import { CountryComponent } from './country/country.component';
 import { UsersComponent } from './users/users.component';
+import {MatListModule} from "@angular/material/list";
+import { MainNavigationComponent } from './main-navigation/main-navigation.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from "./app-routing.module";
+import { UserTableComponent } from './user-table/user-table.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CountryComponent,
-    UsersComponent
+    UsersComponent,
+    MainNavigationComponent,
+    UserTableComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatListModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
