0041f627e14a3bb849e382f78b8a2e7b5160e50a
Flo <florian.wirbser@gmail.com>
2023-05-06 10:15:18 +0200

    - add button with same design like on voting table to navigate back to voting table - reduce dashboard to 3 cards and set background

--- client/src/app/esc-dashboard/esc-dashboard.component.ts
+++ client/src/app/esc-dashboard/esc-dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-esc-dashboard',
@@ -10,24 +11,30 @@ import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 export class EscDashboardComponent {
   /** Based on the screen size, switch from standard to one column per row */
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
+    map(({matches}) => {
       if (matches) {
         return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
+          {title: 'Card 1', cols: 1, rows: 1},
+          {title: 'Card 2', cols: 1, rows: 1},
+          {title: 'Card 3', cols: 1, rows: 1},
         ];
       }
 
       return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
+        {title: 'Card 1', cols: 1, rows: 1},
+        {title: 'Card 2', cols: 1, rows: 1},
+        {title: 'Card 3', cols: 2, rows: 1},
       ];
     })
   );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver,
+              private router: Router) {
+  }
+
+  toVote() {
+
+    this.router.navigateByUrl("/vote");
+  }
+
 }
