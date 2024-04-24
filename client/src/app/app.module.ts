2e91e189aaf04c36ef287a8b5b4a31be7e54fb27
dr_schopalopp <dr_schopalopp@gmx.de>
2024-04-24 23:24:09 +0200

    refactor: remove unnecessary module imports from AppModule

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,20 +1,5 @@
-import { LayoutModule } from '@angular/cdk/layout';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatSortModule } from '@angular/material/sort';
-import { MatTableModule } from '@angular/material/table';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
@@ -24,28 +9,12 @@ import { AppComponent } from './app.component';
 @NgModule({
     declarations: [AppComponent],
     imports: [
-        BrowserModule,
-        FormsModule,
-        HttpClientModule,
-        MatListModule,
-        LayoutModule,
-        MatToolbarModule,
-        MatButtonModule,
-        MatSidenavModule,
-        MatIconModule,
-        BrowserAnimationsModule,
         AppRoutingModule,
-        MatTableModule,
-        MatPaginatorModule,
-        MatSortModule,
-        MatInputModule,
-        MatCardModule,
-        MatSelectModule,
-        MatGridListModule,
-        MatMenuModule
 
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
     ],
-    providers: [],
     bootstrap: [AppComponent]
 })
 export class AppModule {
