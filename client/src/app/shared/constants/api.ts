0c3ab9d84d33db04776e112853bb56c6434af586
Flo <florian.wirbser@gmail.com>
2024-05-07 16:18:24 +0200

    feat: try to change ui to tile design

--- client/src/app/shared/constants/api.ts
+++ client/src/app/shared/constants/api.ts
@@ -1,7 +1,7 @@
 import { HttpHeaders } from '@angular/common/http';
 import { isDevMode } from '@angular/core';
 
-export const API_HOST = isDevMode() ? 'http://localhost:8080' : 'https://api.esc-pub.dev.azmedia-web.de';
+export const API_HOST = isDevMode() ? 'https://api.esc-pub.dev.azmedia-web.de' : 'https://api.esc-pub.dev.azmedia-web.de';
 
 export const httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
