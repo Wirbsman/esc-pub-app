e77c7ba44d9869e3bbd42c26e03ea68d9e70c5ed
dr_schopalopp <dr_schopalopp@gmx.de>
2024-05-11 12:03:37 +0200

    refactor: determine API_HOST by window.location.host

--- client/src/app/shared/constants/api.ts
+++ client/src/app/shared/constants/api.ts
@@ -1,7 +1,7 @@
 import { HttpHeaders } from '@angular/common/http';
 import { isDevMode } from '@angular/core';
 
-export const API_HOST = isDevMode() ? 'https://api.esc-pub.dev.azmedia-web.de' : 'https://api.esc-pub.dev.azmedia-web.de';
+export const API_HOST = isDevMode() ? 'http://localhost:8080' : `https://api.${window.location.host}`;
 
 export const httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
